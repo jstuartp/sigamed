bf4d4667badf939a7dcc1f1e07b1b8a71cd571d8
Stuart <jstuartp@gmail.com>
2022-06-07 08:20:13 -0600

    Actualización 6/6
    Mailer de cargas y modificaciones en correos

--- public/assets/js/tek_exp_students.js
+++ public/assets/js/tek_exp_students.js
@@ -2275,6 +2275,8 @@ Tecnotek.Charges = {
                     Tecnotek.showErrorMessage(data.message,true, "", false);
                 } else {
                     $.fancybox.close();
+                    alert("Se ha enviado la Carga al Profesor.");
+                    location.reload(true);
                     Tecnotek.Charges.searchCharges();
                 }
             },
